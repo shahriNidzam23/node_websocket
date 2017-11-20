94512b0782570769f9371581aea463e7db0e7607
Shahrin Nidzam <shahrin.nidzam@gmail.com>
2017-11-20 08:17:18 +0800

    deploy websocket

---
+++ server.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+var clients = [];
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('message', function(message){
+  	console.log(message);
+  	wss.clients.forEach((client) => {
+    	client.send(message);
+  	});
+      // for (var i=0; i < clients.length; i++) {
+      //   clients[i].send(message.utf8Data);
+      // }
+  });
+  ws.on('close', () => console.log('Client disconnected'));
+});
